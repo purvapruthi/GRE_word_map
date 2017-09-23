50d79ea9b95ef3c30956a3c3bf2a6c01473d562b
purvapruthi <purvapruthi617@gmail.com>
2017-09-23 20:13:08 +0530

    Renaming files

--- words.js
+++ words.js
@@ -7,7 +7,7 @@ var vis = d3.select("#chart")
     .attr("width", w)
     .attr("height", h);
 
-d3.json("force.json", function(json) {
+d3.json("words.json", function(json) {
   var force = d3.layout.force()
       .charge(-120)
       .linkDistance(30)
